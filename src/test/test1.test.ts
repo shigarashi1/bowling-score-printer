42ef503b343b1e7c1f81cf986d45979945d0b13a
50ra4 <50ra4@MacBook-Pro.local>
2020-06-13 22:43:41 +0900

    ADD: test1 file

---
+++ src/test/test1.test.ts
@@ -0,0 +1,28 @@
+// import { readTextFileLines } from '../lib/utils';
+// const lines = readTextFileLines('src/.test-data/test1.txt', 'utf8');
+
+const TEST_CASE_NAME = 'テストケース1';
+describe(`${TEST_CASE_NAME} 1.レーン合計を計算し出力する`, () => {
+  it('合計を返却', () => {
+    // expect(1586).toBe();
+  });
+});
+describe(`${TEST_CASE_NAME} 2.プレイヤーをゲームの合計スコアの昇順で出力する`, () => {
+  it.todo('昇順で出力');
+});
+describe(`${TEST_CASE_NAME} 3.プレイヤーごとのゲームの合計スコアと平均スコアを出力する`, () => {
+  it.todo('プレイヤー1');
+  it.todo('プレイヤー2');
+});
+describe(`${TEST_CASE_NAME} 4.プレイヤーごとのゲームのストライク数、スペア数、ガター数を出力する`, () => {
+  it.todo('プレイヤー1');
+  it.todo('プレイヤー2');
+});
+describe(`${TEST_CASE_NAME} 5.プレイヤー名、ゲーム数、フレーム数、何投目かを指定して、倒したピンの数を出力する`, () => {
+  it.todo('プレイヤー1');
+  it.todo('プレイヤー2');
+});
+describe(`${TEST_CASE_NAME} 6.プレイヤーごとのスコアシートを作成し、出力する`, () => {
+  it.todo('プレイヤー1');
+  it.todo('プレイヤー2');
+});
