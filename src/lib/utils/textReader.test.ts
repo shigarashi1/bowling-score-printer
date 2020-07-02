b4339102883f6e06b78956514c2d50441c779d2f
sigarashi <48591598+shigarashi1@users.noreply.github.com>
2020-07-02 22:28:14 +0900

    FIXED: refactor (#18)
    * FIXED: lint comment
    
    * FIXED: refactor
    
    * FIXED: readme
    
    * FIXED: normarizeLines
    
    Co-authored-by: 50ra4 <50ra4@MacBook-Pro.local>

--- src/lib/utils/textReader.test.ts
+++ src/lib/utils/textReader.test.ts
@@ -2,7 +2,7 @@ import { readTextFile } from './textReader';
 
 describe('readTextFileLines', () => {
   it('行を文字列の配列として返却', () => {
-    const lines = readTextFile('src/lib/utils/textReader.test.txt', 'utf8');
+    const lines = readTextFile('\n')('src/lib/utils/textReader.test.txt', 'utf8');
     lines.forEach((line) => {
       expect(typeof line === 'string').toBeTruthy();
     });
--- src/lib/utils/textReader.ts
+++ src/lib/utils/textReader.ts
@@ -3,9 +3,13 @@ import { pipe, split } from 'ramda';
 
 const toString = (v: unknown) => String(v);
 
+type Params = Parameters<typeof readFileSync>;
+type Path = Params[0];
+type Options = Params[1];
+
 /**
  * テキストファイルを読み込み、行を文字列の配列として返却する
- * @param path テキストファイルのパス（絶対パス）
- * @param encode エンコード
+ * @param linefeedStr 改行コード
  */
-export const readTextFile = pipe(readFileSync, toString, split('\n'));
+export const readTextFile = (linefeedStr: string): ((path: Path, options: Options) => string[]) =>
+  pipe(readFileSync, toString, split(linefeedStr));
